aef9bdc97d1614f95a9939f7fbcfae3475cae559
saad <saadiqbal19114@gmail.com>
2024-02-28 18:15:21 +0500

    setup react project

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark mt-auto">
+      <h1 className="text-white text-center p-4">
+        All Right Reserved &copy; MSI
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
