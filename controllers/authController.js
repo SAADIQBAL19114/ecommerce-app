6f97418af2d0fcce2e467e90997870e0eeeb49e5
saad <saadiqbal19114@gmail.com>
2024-03-04 20:23:25 +0500

    added some packages and edited register api

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
       return res.send({ message: "Email is Required" });
@@ -25,7 +25,9 @@ const registerController = async (req, res) => {
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
       return res.status(400).json({
+        success: false,
         message: "User already exist",
+        user
       });
     }
     const hashedPassword = await hashPassword(password);
@@ -37,8 +39,9 @@ const registerController = async (req, res) => {
       address,
     });
     res.status(201).json({
+      success: true,
       message: "User Registered",
-      data: user,
+      user,
     });
   } catch (error) {
     console.log(error);
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const authRoutes = require("./routes/authRoute.js");
-const productRoutes = require("./routes/productRoutes.js");
-const categoryRoutes = require("./routes/categoryRoute.js");
+const express = require ("express")
+const colors = require("colors")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const authRoutes =require("./routes/authRoute.js")
 const { sequelize } = require("./sequelize/models");
+const cors = require("cors");
 
 // configure env
 dotenv.config();
@@ -14,14 +13,12 @@ dotenv.config();
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/product", productRoutes);
-app.use("/api/v1/category", categoryRoutes);
-
 // rest api
 app.get("/", (req, res) => {
   res.send({
@@ -33,13 +30,11 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 // listen
-app.listen(PORT, async () => {
+app.listen(PORT, async() => {
   console.log(
     `server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .black
   );
-  await sequelize.sync({ alter: true });
-  console.log("All models were synchronized successfully.");
-  // await sequelize.authenticate()
-  // console.log("Data base connected".bgRed .brightWhite);
+  await sequelize.authenticate()
+  console.log("Data base connected".bgRed .brightWhite);
 });
