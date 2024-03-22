6cf76e439bf0d308988ec6839be04b41d3d9c13d
saad <saadiqbal19114@gmail.com>
2024-03-22 16:11:59 +0500

    search functionality and started ProductDetails

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout.js";
 import { useAuth } from "../context/auth.js";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { Checkbox, Radio } from "antd";
@@ -9,11 +9,13 @@ import { Prices } from "../components/Prices.js";
 
 const HomePage = () => {
   // eslint-disable-next-line
+  const navigate = useNavigate();
   const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
   const [radio, setRadio] = useState([]);
+  const [search, setSearch] = useState("");
 
   const getAllProducts = async () => {
     try {
@@ -39,19 +41,6 @@ const HomePage = () => {
       toast.error("Something went wrong in getting categories");
     }
   };
-
-  // get filtered Product
-  // const filterProduct = async () => {
-  //   try {
-  //     const { data } = await axios.post("/api/v1/product/product-filter", {
-  //       checked,
-  //       radio,
-  //     });
-  //     setProducts(data?.products);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   // filter by category
   const handleFilter = (value, id) => {
@@ -88,10 +77,14 @@ const HomePage = () => {
     }
   }, [checked, radio]);
 
+  const filteredProducts = products.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <Layout>
       <div className="row mt-3">
-        <div className="col-md-3">
+        <div className="col-md-3 mt-5 pt-3">
           <h4 className="text-center">Filter By Category</h4>
           <div className="d-flex flex-column p-4">
             {categories?.map((c) => (
@@ -123,31 +116,89 @@ const HomePage = () => {
           </div>
         </div>
         <div className="col-md-9">
-          <h1 className="text-center">All Products</h1>
-          <div className="d-flex flex-wrap">
-            {products?.map((p) => (
-              <div className="card m-2" style={{ width: "18rem" }}>
-                <div>
-                  <img
-                    src={`${p.image}`}
-                    className="card-img-top w-100"
-                    style={{
-                      objectFit: "contain",
-                      maxHeight: "200px",
-                      padding: "10px",
-                    }}
-                    alt={p.name}
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <h1 className="">All Products</h1>
+            </div>
+            <div className="col-md-4">
+              <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+                <div className="input-group">
+                  <label htmlFor="search" className="visually-hidden">
+                    Search
+                  </label>
+                  <input
+                    id="search"
+                    type="text"
+                    role="searchbox"
+                    className="form-control"
+                    placeholder="Search Items"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                   />
+                  <button className="btn btn-outline-secondary" type="submit">
+                    Search
+                  </button>
                 </div>
-                <div className="card-body">
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description}</p>
-                  <p className="card-text">$ {p.price}</p>
-                  <button class="btn btn-primary ms-1">More Details</button>
-                  <button class="btn btn-secondary ms-1">Add to Cart</button>
-                </div>
-              </div>
-            ))}
+              </form>
+            </div>
+          </div>
+          <div className="d-flex flex-wrap">
+            {search === ""
+              ? products?.map((p) => (
+                  <div className="card m-2" style={{ width: "18rem" }}>
+                    <div>
+                      <img
+                        src={`${p.image}`}
+                        className="card-img-top w-100"
+                        style={{
+                          objectFit: "contain",
+                          maxHeight: "200px",
+                          padding: "10px",
+                        }}
+                        alt={p.name}
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text">{p.description}</p>
+                      <p className="card-text">$ {p.price}</p>
+                      <button
+                        class="btn btn-primary ms-1"
+                        onClick={() => navigate(`/product/${p.id}`)}
+                      >
+                        More Details
+                      </button>
+                      <button class="btn btn-secondary ms-1">
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                ))
+              : filteredProducts?.map((p) => (
+                  <div className="card m-2" style={{ width: "18rem" }}>
+                    <div>
+                      <img
+                        src={`${p.image}`}
+                        className="card-img-top w-100"
+                        style={{
+                          objectFit: "contain",
+                          maxHeight: "200px",
+                          padding: "10px",
+                        }}
+                        alt={p.name}
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text">{p.description}</p>
+                      <p className="card-text">$ {p.price}</p>
+                      <button class="btn btn-primary ms-1">More Details</button>
+                      <button class="btn btn-secondary ms-1">
+                        Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                ))}
           </div>
         </div>
       </div>
