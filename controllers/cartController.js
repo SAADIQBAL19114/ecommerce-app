f5d3b9f6bf04c78f273f92d153fd7d44b548b0d2
saad <saadiqbal19114@gmail.com>
2024-04-25 12:41:26 +0500

    cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,37 +3,25 @@ const { Product, User, Cart } = require("../sequelize/models");
 const addToCart = async (req, res) => {
   try {
     const { pId, uId } = req.params;
-     const product = await Product.findByPk(pId);
-     if (!product) {
-       return res.status(404).json({ error: "Product not found" });
-     }
-     const user = await User.findByPk(uId);
-     if (!user) {
-       return res.status(404).json({ error: "User not found" });
-     }
-     const existingCartItem = await Cart.findOne({
-       where: {
-         userId: uId,
-         productId: pId,
-       },
-     });
 
-     if (existingCartItem) {
-       return res
-         .status(400)
-         .json({ error: "Product already exists in the cart" });
-     }
+    const product = await Product.findOne({ where: { id: pId } });
+    console.log("product", product);
+    const user = await User.findOne({ where: { id: uId } });
+    console.log("user", user);
 
+    if (!user || !product) {
+      return res.status(404).json({ error: "User or category not found." });
+    }
+
+    // Add item to cart
     const cartItem = await Cart.create({
-      userId: uId,
-      productId: pId,
-      quantity: 1,
+      userId: user.id,
+      productId: product.id,
+      quantity: product.quantity,
+      Total: 100,
     });
-    res.status(201).json({
-      success: true,
-      message: "Cart Created",
-      cartItem,
-    });
+
+    res.status(201).json(cartItem);
   } catch (error) {
     console.error("Error adding item to cart:", error);
     res.status(500).json({ error: "Internal server error" });
